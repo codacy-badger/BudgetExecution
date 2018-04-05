a4232cbecda36b661029a8fad99fcbcc958ff367
Terry Eppler <terry.eppler@gmail.com>
2018-04-05 06:39:42 -0500

    BUILD

---
+++ Database/sql/CREATE_TABLE_T6.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "T6" ( `BudgetLevel` TEXT, `DocType` TEXT, `RPIO` TEXT, `Org` TEXT, `BFY` TEXT, `Fund` TEXT, `RC` TEXT, `TCN` TEXT, `Qtr` TEXT, `Date` TEXT, `Code` TEXT, `ProgramProjectCode` TEXT, `ProgramProjectName` TEXT, `NpmCode` TEXT, `FromTo` TEXT, `BOC` TEXT, `Amount` NUMERIC );
